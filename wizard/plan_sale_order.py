d53b14cef7ebdb6cdb47fd254e16c6bef451976c
Anh Tuan <silerspirit2001@gmail.com>
2022-09-19 11:31:51 +0700

    b3

--- wizard/plan_sale_order.py
+++ wizard/plan_sale_order.py
@@ -10,15 +10,12 @@ class PlanSaleOrder(models.Model):
     name = fields.Char(required=True)
     order_id = fields.Many2one('sale.order', readonly=True)
     business_plan_info = fields.Char(required=True)
-
     state = fields.Selection(
         [('new', 'New'),
          ('process', 'Process'),
          ('approved', 'Approved'),
          ('reject', 'Reject')],
         'State', default="new")
-
-    # approver_id = fields.Many2one('res.partner', string='Approver')self
     plan_line = fields.One2many('plan.sale.order.line', 'plan_id', string='Plan Lines',
                                 states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True,
                                 auto_join=True)
@@ -30,7 +27,6 @@ class PlanSaleOrder(models.Model):
                           )
         self.write({'state': 'process'})
         self.order_id.write({'plan_id': self.id})
-
 
     # @api.model
     # def create(self, vals):
